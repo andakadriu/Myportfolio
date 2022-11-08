64fb131d26ad57c6377bb108f175bbe040e7ed46
Anda Kadriu <andakadriu20@gmail.com>
2022-11-08 01:05:29 -0800

    first commit

---
+++ home.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width" initial-scale="1.0">
+    <meta charset="UTF-8">
+    <title>Home</title>
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<section class="header">
+    <a href="home.php" class="logo">anda.</a>
+
+    <nav class="navbar">
+        <a href="home.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="reserve.php">Reserve</a>
+        <a href="home.php"></a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars">
+
+    </div>
+</section>
+<section class="services">
+    <h1 class="heading-tittle">My work </h1>
+    <div class="box-container">
+        <div class="box">
+            <img src="images/img1.png" alt="">
+            <h3>Keychain</h3>
+        </div>
+        <div class="box">
+            <img src="images/img2.png" alt="">
+            <h3>Jewerly Bowl</h3>
+        </div>
+        <div class="box">
+            <img src="images/img1.png" alt="">
+            <h3>Letter A Heart keychain</h3>
+        </div>
+        <div class="box">
+            <img src="images/img4.png" alt="">
+            <h3>Jewerly Plate</h3>
+        </div>
+        <div class="box">
+            <img src="images/img5.png" alt="">
+            <h3>Bowl ocean art</h3>
+        </div>
+
+
+    </div>
+</section>
+
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"><i class="fas fa-angle-right"></i> Home</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i> About</a>
+            <a href="reserve.php"><i class="fas fa-angle-right"></i> Reserve</a>
+            <a href="home.php"><i class="fas fa-angle-right"></i> </a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="#"><i class="fas fa-angle-right"></i> ask questions</a>
+            <a href="#"><i class="fas fa-angle-right"></i> About us</a>
+            <a href="#"><i class="fas fa-angle-right"></i> Privacy policy</a>
+            <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info </h3>
+            <a href="#"><i class="fas fa-phone"></i> +38349649828</a>
+            <a href="#"><i class="fas fa-envelope"></i> andakadriu20@gmail.com</a>
+            <a href="#"><i class="fas fa-map"></i> Fushe Kosove,Kosove</a>
+
+
+        </div>
+
+        <div class="box">
+            <h3>Follow me</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+            <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i>Linkedin</a>
+            <a href="#"><i class="fab fa-github"></i>Github</a>
+        </div>
+    </div>
+
+    <div class="credit">Credit by <span>Anda</span> All right reserved!</div>
+
+</section>
+
+<script src="js/script.js"></script>
+</body>
+</html>
---
+++ reserve.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width" initial-scale="1.0">
+    <meta charset="UTF-8">
+    <title>Reserve</title>
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<section class="header">
+    <a href="home.php" class="logo">anda.</a>
+
+    <nav class="navbar">
+        <a href="home.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="reserve.php">Reserve</a>
+        <a href="home.php"></a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars">
+
+    </div>
+</section>
+
+<div class="heading" style="background:url(images/heading1.png) no-repeat">
+    <h1>Reserve yours Now!</h1>
+</div>
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"><i class="fas fa-angle-right"></i> Home</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i> About</a>
+            <a href="reserve.php"><i class="fas fa-angle-right"></i> Reserve</a>
+            <a href="home.php"><i class="fas fa-angle-right"></i> </a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="#"><i class="fas fa-angle-right"></i> ask questions</a>
+            <a href="#"><i class="fas fa-angle-right"></i> About us</a>
+            <a href="#"><i class="fas fa-angle-right"></i> Privacy policy</a>
+            <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info </h3>
+            <a href="#"><i class="fas fa-phone"></i> +38349649828</a>
+            <a href="#"><i class="fas fa-envelope"></i> andakadriu20@gmail.com</a>
+            <a href="#"><i class="fas fa-map"></i> Fushe Kosove,Kosove</a>
+
+
+        </div>
+
+        <div class="box">
+            <h3>Follow me</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+            <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i>Linkedin</a>
+            <a href="#"><i class="fab fa-github"></i>Github</a>
+        </div>
+    </div>
+
+    <div class="credit">Credit by <span>Anda</span> All right reserved!</div>
+
+</section>
+
+<script src="js/script.js"></script>
+</body>
+</html>
